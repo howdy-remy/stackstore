1da563a052e18bad1a5bffe4e9cb3a8274d892e9
Kimber Lynch <kmlynch714@gmail.com>
2016-06-06 18:04:22 -0400

    #1 create seedfile with users and products

--- seed.js
+++ seed.js
@@ -20,38 +20,50 @@ name in the environment files.
 var chalk = require('chalk');
 var db = require('./server/db');
 var User = db.model('user');
+var Product = db.model('product');
 var Promise = require('sequelize').Promise;
 
-var seedUsers = function () {
 
-    var users = [
-        {
-            email: 'testing@fsa.com',
-            password: 'password'
-        },
-        {
-            email: 'obama@gmail.com',
-            password: 'potus'
-        }
-    ];
-
-    var creatingUsers = users.map(function (userObj) {
-        return User.create(userObj);
-    });
-
-    return Promise.all(creatingUsers);
-
+var data = {
+	user: [{
+		email: 'testing@fsa.com',
+		password: 'password'
+	}, {
+		email: 'obama@gmail.com',
+		password: 'potus'
+	}],
+	product: [{
+		title: 'Decoy Detonator',
+		description: "Need to create a diversion to sneak past some Ministry employees? Then here's just the thing! You can twist the key at the top to set it up. Rolling action is friction activated. You just pull it back, release and off it goes.",
+		price: 34.90,
+		quantity: 10,
+		category: ['diversion'],
+		photoUrl: 'http://imgur.com/a/cWeLU'
+	}, {
+		title: 'Test Product',
+		description: "Need to create a diversion to sneak past some Ministry employees? Then here's just the thing! You can twist the key at the top to set it up. Rolling action is friction activated. You just pull it back, release and off it goes.",
+		price: 34.90,
+		quantity: 10,
+		category: ['diversion', 'office'],
+		photoUrl: 'http://imgur.com/a/cWeLU'
+	}]
 };
 
 db.sync({ force: true })
-    .then(function () {
-        return seedUsers();
-    })
-    .then(function () {
-        console.log(chalk.green('Seed successful!'));
-        process.kill(0);
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.kill(1);
-    });
+	.then(function () {
+		return Promise.map(Object.keys(data), function (name) {
+			return Promise.map(data[name], function (item) {
+				return db.model(name)
+					.create(item);
+			});
+		});
+	})
+	.then(function () {
+		console.log(chalk.green('Seed successful!'));
+		process.kill(0);
+	})
+	.catch(function (err) {
+		console.error(err);
+		console.log('hello!');
+		process.kill(1);
+	});
