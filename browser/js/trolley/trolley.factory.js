a6d30b153bcf83e2ee3cd67f90bff9e4ad00d6b1
Kimber Lynch <kmlynch714@gmail.com>
2016-06-08 16:49:00 -0400

    #9 add cart quantity updating

--- browser/js/trolley/trolley.factory.js
+++ browser/js/trolley/trolley.factory.js
@@ -5,18 +5,15 @@ app.factory('TrolleyFactory', function($http, $state){
 			.then(res => res.data);
 		}, 
 		addToCart: function(itemToBeAdded){
-			console.log('item in the factory', itemToBeAdded);
 			return $http.post('/api/trolley', itemToBeAdded)
 			.then(function(addedItem){
 				$state.go('trolley');
 			});
 		},
 		updateCart: function(itemToBeUpdated, quantity){
-			console.log('in the updateCart', 'id', itemToBeUpdated.id, 'quantity', quantity);
 			return $http.put('/api/trolley', {id: itemToBeUpdated.id, quantity: quantity});
 		}, 
 		removeFromCart: function(itemToBeRemoved){
-			console.log('in the remove from cart', itemToBeRemoved)
 			return $http.delete('/api/trolley/'+ itemToBeRemoved.id);
 		}
 	};
--- server/app/routes/trolley.js
+++ server/app/routes/trolley.js
@@ -2,36 +2,30 @@
 var router = require('express').Router();
 module.exports = router;
 
+//getting all items in the trolley from the session
+//check if persists after user login!!!!!!!
+router.get('/', function(req, res, next){
+	console.log('the trolley in the main get', req.session.trolley);
+	res.send(req.session.trolley);
+});
+
 //adding an item to the trolley
 router.post('/', function(req, res, next){
-	console.log('hit the post route')
-	console.log('req.body', req.body);
-	//need to test with a client********//
 	if(!req.session.trolley){
 		req.session.trolley = [];
 	}
 	req.session.trolley.push(req.body);
-	console.log('here is our trolley after push', req.session.trolley, '-----------------------');
 	res.sendStatus(201);
-});
-
-//getting all items in the trolley from the session
-router.get('/', function(req, res, next){
-	console.log('the trolley in the main get', req.session.trolley)
-	res.send(req.session.trolley);
 });
 
 ///to update quantity in cart
 router.put('/', function(req, res, next){ 
-	console.log('in the put route', req);//
-	let index;
+	let index = null;
 	req.session.trolley.forEach(function(item, i){
-		if(item.id === req.body.id){
-			index = i;
-		}
+		if(item.id === req.body.id){ index = i; }
 	});
-	if(index) {
-		req.session.trolley[index].quantity = req.body.quantity;
+	if(index !== null) {
+		req.session.trolley[index].quantity = req.body.quantity[req.body.id];
 		res.sendStatus(204);
 	} else {
 		res.sendStatus(404); 
@@ -40,13 +34,8 @@ router.put('/', function(req, res, next){
 
 ///to delete an item from the cart
 router.delete('/:id', function(req, res, next){ 
-	console.log('in here', req.params.id)
 	req.session.trolley = req.session.trolley.filter(function(item){
 		return item.id != req.params.id;
 	});
-	console.log('session after filter', req.session.trolley);
 	res.sendStatus(204);
-	// find the specific item in the session.trolley = use filter to find the 
-	// item and then return an array without that item, set this equal to the 
-	// session.trolly
 });
