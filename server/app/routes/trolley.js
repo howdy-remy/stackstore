3c569b82eeaf076a9b363f46528335916d2984af
Kimber Lynch <kmlynch714@gmail.com>
2016-06-08 18:41:12 -0400

    fix cart to not allow duplicate items and  update quantity instead

--- server/app/routes/trolley.js
+++ server/app/routes/trolley.js
@@ -12,10 +12,20 @@ router.get('/', function(req, res, next){
 //adding an item to the trolley
 router.post('/', function(req, res, next){
 	req.body.amount = req.body.amount ? req.body.amount+1 : 1; //amount of product wanted (add only one to cart at start)
+	let found = false;
+
 	if(!req.session.trolley){
 		req.session.trolley = [];
+	} else {
+		for(let i =0; i < req.session.trolley.length; i++){
+			if(req.session.trolley[i].id === req.body.id){
+				req.session.trolley[i].amount++;
+				found = true;
+			}
+		}
 	}
-	req.session.trolley.push(req.body);
+
+	if(!found) req.session.trolley.push(req.body);
 	res.sendStatus(201);
 });
 
