9ea838a16b958d0e6df10604721c3051ba6ad999
Kimber Lynch <kmlynch714@gmail.com>
2016-06-09 13:09:32 -0400

    add testing for user signup

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -8,28 +8,34 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('signupCtrl', function ($scope, AuthService, $state, signUpFactory) {
+app.controller('signupCtrl', function ($scope, AuthService, $state) {
 
     $scope.signup = {};
     $scope.error = null;
 
     $scope.sendSignup = function(signupInfo){
-        return signUpFactory.sendSignup(signupInfo);
+        AuthService.signup(signupInfo)
+        .then(function(){
+            $state.go('products');
+        })
+        .catch(function () {
+            $scope.error = 'Invalid login credentials.';
+        });
     };
 
 });
 
-app.factory('signUpFactory', function($http, $state, AuthService){
-    return {
-        sendSignup: function(signupInfo){
-            return $http.post('/signup', signupInfo)
-            .then(function(response){
-                return AuthService.getLoggedInUser(false);
-            })
-            .then(function(){
-                $state.go('products');
-            })
-            .catch(console.error);
-        }
-    };
-});
+// app.factory('signUpFactory', function($http, $state, AuthService){
+//     return {
+//         sendSignup: function(signupInfo){
+//             return $http.post('/signup', signupInfo)
+//             .then(function(response){
+//                 return AuthService.getLoggedInUser(false);
+//             })
+//             .then(function(){
+//                 $state.go('products');
+//             })
+//             .catch(console.error);
+//         }
+//     };
+// });
--- tests/browser/fsa-prebuilt/auth-service-test.js
+++ tests/browser/fsa-prebuilt/auth-service-test.js
@@ -285,4 +285,127 @@ describe('AuthService', function () {
 
     });
 
+    describe('signup', function(){
+
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should make a request POST /signup with the given data', function (done) {
+
+            var newuser = {
+                email: 'newuser@email.com',
+                password: '12345'
+            };
+
+            $httpBackend.expectPOST('/signup', newuser).respond(200, {});
+
+            AuthService.signup(newuser).then(done);
+
+            $httpBackend.flush();
+
+        });
+
+        it('should forward invalid credentials error on unsuccessful response', function (done) {
+
+            $httpBackend.expectPOST('/signup').respond(401);
+
+            AuthService.signup({ email: 'obama@gmail.com', password: 'nop' })
+            .catch(function (err) {
+                expect(err.message).to.be.equal('Invalid sign up credentials.');
+                done();
+            });
+
+            $httpBackend.flush();
+
+        });
+
+        describe('on successful response', function () {
+            var user = {email: 'coolguy@beans.com'};
+            var login = {email: 'coolguy@beans.com', password: 'sweetjuicy'};
+
+            beforeEach(function () {
+                $httpBackend.whenPOST('/signup').respond({user: user});
+            });
+
+        it('should resolve to the responded user', function (done) {
+
+                AuthService.signup(login).then(function (user) {
+                    expect(user).to.be.deep.equal(user);
+                    done();
+                });
+
+                $httpBackend.flush();
+
+            });
+
+            it('should set Session', function (done) {
+
+                Session.destroy();
+
+                AuthService.signup(login).then(function (user) {
+                    expect(Session.user).to.be.deep.equal(user);
+                    done();
+                });
+
+                $httpBackend.flush();
+
+            });
+
+            it('should fire off AUTH_EVENTS.loginSuccess', function (done) {
+
+                var spy = sinon.spy();
+
+                $rootScope.$on(AUTH_EVENTS.loginSuccess, spy);
+
+                AuthService.signup(login).then(function () {
+                    expect(spy.called).to.be.ok;
+                    done();
+                });
+
+                $httpBackend.flush();
+
+            });
+        });
+
+
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
