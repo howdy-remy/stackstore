319edaa25e72047a920969e52263334a94a1f3f0
Rina Krevat <krevat@seas.upenn.edu>
2016-06-10 13:43:49 -0400

    #50 added review routes and edited product.factory to accommodate new route

--- server/app/routes/reviews.js
+++ server/app/routes/reviews.js
@@ -2,20 +2,41 @@
 var router = require('express').Router();
 var Review = require('../../db/models/review.js');
 var Index = require('../../db/index.js');
+var User = require('../../db/models/user.js');
 module.exports = router;
 
-//getting all reviews
-router.get('/:productId', function(req, res, next){
-	Review.findAll({where: {productId: req.params.productId}})
+// //getting all reviews
+// router.get('/:productId', function(req, res, next){
+// 	Review.findAll({where: {productId: req.params.productId}})
+// 	.then(function(reviews){
+// 		res.send(reviews);
+// 	})
+// 	.catch(next);
+// });
+
+// //getting a specific review
+// router.get('/:productId/:id', function (req, res, next){
+// 	Review.findById(req.params.id)
+// 	.then(function(review){
+// 		res.send(review);
+// 	})
+// 	.catch(next);
+// });
+
+//get all reviews
+router.get('/', function(req, res, next){
+	var whereObj = Object.keys(req.query).length ? req.query: {};	
+
+	Review.findAll(whereObj)
 	.then(function(reviews){
 		res.send(reviews);
 	})
 	.catch(next);
 });
 
-//getting a specific review
-router.get('/:productId/:id', function (req, res, next){
-	Review.findById(req.params.id)
+//get a specific review
+router.get('/:reviewId', function(req, res, next){
+	Review.findById(req.params.reviewId)
 	.then(function(review){
 		res.send(review);
 	})
