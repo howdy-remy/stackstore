a15b00d4c257e17c1e611dc2f03b90eb50937cd4
Mallory Payne <mallory.payne18@gmail.com>
2016-06-10 14:55:10 -0400

    updating orders route to allow for updating on checkout

--- server/app/routes/orders.js
+++ server/app/routes/orders.js
@@ -2,6 +2,7 @@
 var router = require('express').Router();
 module.exports = router;
 var Orders = require('../../db/models/order.js');
+var OrderProducts = require('../../db/models/orderProducts.js')
 
 
 //adding an item to the trolley
@@ -10,17 +11,25 @@ router.post('/checkout', function(req, res, next){
 	console.log('here is my req body', req.body);
 	///**** fix the format of what is in the .create!!!!!****
 	Orders.create({
-		items: req.session.trolley,
 		street: req.body.street,
 		city: req.body.city,
 		state: req.body.state,
 		zip: req.body.zip,
-		status: 'placed',
 		email: req.body.email,
 		firstName: req.body.firstName,
 		lastName: req.body.lastName
 		})  
 	.then(function(createdOrder){
+		console.log('this should be the order id of the order just created ', createdOrder.id);
+		OrderProducts.bulkCreate({
+			productId: createdOrder.id,
+			amount: 10,
+			price: 10
+		})
+		.then(function(createdOrderProducts){
+///need to update the products model to update the quantity
+		});
+		items: req.session.trolley, ///need to update the items
 		//what do we want to do after we create the order??  Send email to user??
 		res.status(201);
 		
