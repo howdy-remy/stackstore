6008351da97aea0c65a7bd03c2ac5f71751a011a
Rina Krevat <krevat@seas.upenn.edu>
2016-06-14 17:16:27 -0400

    shows total price and removes an item when its quantity is set to zero

--- browser/js/trolley/trolley.controller.js
+++ browser/js/trolley/trolley.controller.js
@@ -3,8 +3,30 @@ app.controller('TrolleyCtrl', function($scope, TrolleyFactory, trolley){
 	$scope.trolley = trolley;
 	$scope.amount = {};
 
+	console.log($scope.trolley)
+
+	var totalPrice = function(){
+		var tot = 0;
+		for (var i = 0; i < $scope.trolley.length; i++) {
+		    tot += (parseFloat($scope.trolley[i].amount) * parseFloat($scope.trolley[i].price));
+		}
+		return tot;
+	}
+
+	$scope.tot = totalPrice();
+
 	$scope.updateCart = function(item){
-		return TrolleyFactory.updateCart(item, $scope.amount);
+
+		if ($scope.amount[item.id] === 0){
+			$scope.removeFromCart(item);
+		}
+		else {
+			var ind = $scope.trolley.indexOf(item);
+			$scope.trolley[ind].amount = $scope.amount[item.id];
+			$scope.tot = totalPrice();
+			 TrolleyFactory.updateCart(item, $scope.amount)
+		}
+
 	};
 
 	$scope.removeFromCart = function(item){
@@ -18,6 +40,9 @@ app.controller('TrolleyCtrl', function($scope, TrolleyFactory, trolley){
 	$scope.checkout = function(){
 		return TrolleyFactory.checkout();
 	};
+
+
+
 });
 
 
