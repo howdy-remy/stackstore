cfb44f108d1507a0cd52a8f4aab450a20d44a602
Rina Krevat <krevat@seas.upenn.edu>
2016-06-14 13:22:13 -0400

    will send an order with order details

--- server/app/routes/orders.js
+++ server/app/routes/orders.js
@@ -7,77 +7,109 @@ var User = require('../../db/models/user.js');
 var Products = require('../../db/models/product.js');
 var Promise = require('bluebird');
 
+var nodemailer = require('nodemailer');
 
-router.post('/checkout', function (req, res, next) {
-	Promise.all([
-			User.findOrCreate({ where: { email: req.body.email } }), //1) find or create user (find if existing user, create if anon user)
-			Order.create(req.body) //2) create order in Orders model with shipping info
-		])
-		.spread(function (user, order) {
-			return order.setUser(user[0]); //associate the order and user
-		})
-		.then(function (order) {
-			return Promise.map(req.session.trolley, function (item) { //2) update Products model to reduce the stock quantities by the number of items ordered
-					return Products.findOne({ where: { id: item.id } })
-						.then(function (originalProduct) {
-							return originalProduct.update({quantity: originalProduct.quantity - item.amount}, { where: { id: item.id } });
-						})
-						.then(function (updatedProduct) { //3) update the price and amount fields in the OrderProducts model
-							return order.addProduct(updatedProduct, { price: item.price, amount: item.amount });
-						});
-				}) //end of promise.map
-				.then(function () {
-					req.session.trolley = []; //4) clear the trolley on the session
-					res.sendStatus(201);
-				})
-				.catch(next);
-		});
+
+router.post('/checkout', function(req, res, next) {
+    Promise.all([
+            User.findOrCreate({ where: { email: req.body.email } }), //1) find or create user (find if existing user, create if anon user)
+            Order.create(req.body) //2) create order in Orders model with shipping info
+        ])
+        .spread(function(user, order) {
+            return order.setUser(user[0]); //associate the order and user
+        })
+        .then(function(order) {
+            Promise.map(req.session.trolley, function(item) { //2) update Products model to reduce the stock quantities by the number of items ordered
+                return Products.findOne({ where: { id: item.id } })
+                    .then(function(originalProduct) {
+                        return originalProduct.update({ quantity: originalProduct.quantity - item.amount }, { where: { id: item.id } });
+                    })
+                    .then(function(updatedProduct) { //3) update the price and amount fields in the OrderProducts model
+                        return order.addProduct(updatedProduct, { price: item.price, amount: item.amount });
+                    });
+                }) //end of promise.map
+                .then(function() {
+                    // set up nodemailer transport
+                    var transporter = nodemailer.createTransport('smtps://wwwgh1604%40gmail.com:gracehopper@smtp.gmail.com');
+                    // console.log("the order is", order)
+                    // console.log("the trolley is", req.session.trolley)
+                    var address = order.dataValues.firstName + " " + order.dataValues.lastName
+                                + "\n" + order.dataValues.street + " \n"
+                                + order.dataValues.city + ", " + order.dataValues.state + " " + order.dataValues.zip;
+                    var orderSum = "";
+                    req.session.trolley.forEach(function(tItem){
+                      orderSum+= "Item: " + tItem.title + "\nQuantity: " + tItem.amount + "@" + tItem.price + " each.\n";
+                    });
+                    var mailOptions = {
+                      from: '"Weasleys Wizard Wheezes 🎩" <wwwgh1604@gmail.com>', // sender address
+                      to: req.body.email, // list of receivers
+                      subject: 'Your order has been placed!', // Subject line
+                      text: 'Hi ' + order.dataValues.firstName + ', your order has been placed and the details are below. Thanks for shopping with us! You will receive another email when your order has shipped.\n\nAddress:\n\n' + address + "\n\nOrder Summary:\n\n" + orderSum // plaintext body
+                      // html: '<b>Hello world 🐴</b>' // html body
+                  };
+
+                  // send mail with defined transport object
+                  transporter.sendMail(mailOptions, function(error, info){
+                      if(error){
+                          return console.log(error);
+                      }
+                      console.log('Message sent: ' + info.response);
+                  });
+
+                    return order;
+                })
+                .then(function() {
+                    req.session.trolley = []; //4) clear the trolley on the session
+                    res.sendStatus(201);
+                })
+                .catch(next);
+        });
 });
 
 // router.post('/email', function (req, res, next) { //to send comfirmation email to user
-// 	//i think we need to npm install nodemailer and use this to send an email
-// 	//we are clearing the trolley before this step, may need to clear it after email is sent
+//  //i think we need to npm install nodemailer and use this to send an email
+//  //we are clearing the trolley before this step, may need to clear it after email is sent
 
 // });
 
 //get all orders
 router.get('/', function (req, res, next) {
 
-	//for query string requesting orders from a specific user
-	var whereObj = Object.keys(req.query).length ? req.query : {};
+  //for query string requesting orders from a specific user
+  var whereObj = Object.keys(req.query).length ? req.query : {};
 
-	Order.findAll({ where: whereObj })
-		.then(function (orders) {
-			res.send(orders);
-		})
-		.catch(next);
+  Order.findAll({ where: whereObj })
+    .then(function (orders) {
+      res.send(orders);
+    })
+    .catch(next);
 
 });
 
 //add a new order
 router.post('/', function (req, res, next) {
 
-	Order.create(req.body)
-		.then(function (createdOrder) {
-			res.status(201).send(createdOrder);
-		})
-		.catch(next);
+  Order.create(req.body)
+    .then(function (createdOrder) {
+      res.status(201).send(createdOrder);
+    })
+    .catch(next);
 
 });
 
 //get a single order
 router.get('/:id', function (req, res, next) {
 
-	Order.findById(req.params.id)
-		.then(function (foundOrder) {
-			if (!foundOrder) {
-				var error = new Error();
-				error.status = 404;
-				throw error;
-			}
-			res.send(foundOrder);
-		})
-		.catch(next);
+  Order.findById(req.params.id)
+    .then(function (foundOrder) {
+      if (!foundOrder) {
+        var error = new Error();
+        error.status = 404;
+        throw error;
+      }
+      res.send(foundOrder);
+    })
+    .catch(next);
 
 });
 
@@ -85,19 +117,19 @@ router.get('/:id', function (req, res, next) {
 //only admins should be able do this!
 router.put('/:id', function (req, res, next) {
 
-	Order.findById(req.params.id)
-		.then(function (foundOrder) {
-			if (!foundOrder) {
-				var error = new Error();
-				error.status = 404;
-				throw error;
-			}
-			return foundOrder.update(req.body);
-		})
-		.then(function (updatedOrder) {
-			res.send(updatedOrder);
-		})
-		.catch(next);
+  Order.findById(req.params.id)
+    .then(function (foundOrder) {
+      if (!foundOrder) {
+        var error = new Error();
+        error.status = 404;
+        throw error;
+      }
+      return foundOrder.update(req.body);
+    })
+    .then(function (updatedOrder) {
+      res.send(updatedOrder);
+    })
+    .catch(next);
 
 });
 
@@ -105,17 +137,17 @@ router.put('/:id', function (req, res, next) {
 //only admins should be able to do this!
 router.delete('/:id', function (req, res, next) {
 
-	Order.findById(req.params.id)
-		.then(function (foundOrder) {
-			if (!foundOrder) {
-				var error = new Error();
-				error.status = 404;
-				throw error;
-			}
-			return foundOrder.destroy();
-		})
-		.then(function () {
-			res.sendStatus(204);
-		})
-		.catch(next);
+  Order.findById(req.params.id)
+    .then(function (foundOrder) {
+      if (!foundOrder) {
+        var error = new Error();
+        error.status = 404;
+        throw error;
+      }
+      return foundOrder.destroy();
+    })
+    .then(function () {
+      res.sendStatus(204);
+    })
+    .catch(next);
 });
